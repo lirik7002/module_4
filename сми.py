2b32841cf1d5fbb8e94b2ab9ebf9e52f0ca2ece5
Кирилл <c.akishin@yandex.ru>
2023-07-09 13:18:23 +0300

    Add files via upload

---
+++ сми.py
@@ -0,0 +1,11 @@
+def naoborot():
+    str = input(f'Ввод')
+    str_revers = ''.join(reversed())
+    if str==str_revers:
+        print("True")
+    else:
+        print("False")
+        
+naoborot()
+
+
