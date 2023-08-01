aeebb3872e55ce9f4297c02223a5169186fe4404
Кирилл <c.akishin@yandex.ru>
2023-08-01 23:59:53 +0300

    Add files via upload

--- app_lesson4/advertisement/advertisement/settings.py
+++ app_lesson4/advertisement/advertisement/settings.py
@@ -78,7 +78,11 @@ WSGI_APPLICATION = 'advertisement.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'test1',
+        'USER': 'MisterBist',
+        'PASSWORD': '',
+        'HOST':'127.0.0.1',
+        'PORT':5432,
     }
 }
 
--- app_lesson4/advertisement/lesson_4/models.py
+++ app_lesson4/advertisement/lesson_4/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db import migrations
+class Advertisement(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название",
+    )
+    description = models.TextField(
+        verbose_name="Описание",
+    )
+    price = models.DecimalField(
+        verbose_name="Цена",
+        max_digits=10,
+        decimal_places=2,
+    )
+    auction = models.BooleanField(
+        verbose_name="Торг",
+        default=False,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата редоктирования"
+    )
 
-# Create your models here.
